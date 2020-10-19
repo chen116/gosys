382112ccd9ada46695b527b8d8adcd6eb4eb4af5
vic <chen116@usc.edu>
2020-10-19 12:36:09 -0700

    a

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,10 +3,22 @@ package main
 import (
 	"fmt"
 	"gosys/pkg/testser"
+	"io"
+	"log"
+	"net/http"
+	"rsc.io/quote"
 )
 
 func main() {
 
-	helloHandler := ser.Serve(3)
-	fmt.Println(helloHandler)
+	res := ser.Serve(3)
+	fmt.Println(res)
+	fmt.Println(quote.Hello())
+	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Hello, world!\n")
+	}
+
+	http.HandleFunc("/hello", helloHandler)
+	log.Println("Listing for requests at http://localhost:8000/hello")
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
